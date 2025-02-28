b6a2a1db61e34b4bc346da0ff9ed0be4eac9975b
Brett Constantinoff <Brett.Constantinoff@catalisgov.com>
2025-02-28 12:00:08 -0700

    initial commit

---
+++ premake5.lua
@@ -0,0 +1,16 @@
+workspace "Grim Tithe"
+    configurations { "Debug", "Release" }
+    startproject "Grim Tithe"
+    flags { "MultiProcessorCompile" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG", "DEBUG_SHADER" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "RELEASE" }
+        optimize "Speed"
+        flags { "LinkTimeOptimization" }
+
+group "Game"
+    include "src/grim-tithe.lua"
---
+++ setup.bat
@@ -0,0 +1,18 @@
+@echo off
+echo Setting up Grim Tithe...
+
+cd /d "%~dp0"
+
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed! Please install Python and ensure it is added to the system PATH.
+    pause
+    exit /b
+)
+
+echo Running Vulkan SDK setup...
+python .\scripts\installVulkan.py
+
+.\external\premake\premake5.exe vs2022
+
+pause
